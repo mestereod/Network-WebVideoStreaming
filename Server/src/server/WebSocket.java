c3ca660205828136075da4e5d463073731d949fb
Kevin <kevinkggo@gmail.com>
2020-09-27 13:53:07 -0300

    Removed Java Client Version and add index.html for client

--- Server/src/server/WebSocket.java
+++ Server/src/server/WebSocket.java
@@ -23,9 +23,9 @@ import java.util.regex.Pattern;
 
 public class WebSocket {
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-		ServerSocket server = new ServerSocket(80);
+		ServerSocket server = new ServerSocket(7777);
 		try {
-			System.out.println("Server has started on 127.0.0.1:80.\r\nWaiting for a connection...");
+			System.out.println("Server has started on port 7777.\r\nWaiting for a connection...");
 			Socket client = server.accept();
 			System.out.println("A client connected.");
                         InputStream in = client.getInputStream();
