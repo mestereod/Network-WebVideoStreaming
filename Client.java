6ae63d869dab0a9b49d4c2639b1b0ba352f15551
RenanErnest <renan.erns@gmail.com>
2020-10-18 00:57:02 -0300

    Implement chat logic and fix image resize

--- Client.java
+++ Client.java
@@ -5,39 +5,40 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.util.UUID;
 
 
 public class Client extends Thread {
 
-	private DataOutputStream out;
 	private DataInputStream in;
+	private JFrame frame;
+	private GUI gui;
 
-	public Client(String serverName, int port ) {
+	public Client(String serverName, int portStreaming, int portChat ) {
+		frame = new JFrame("Streaming");
+		gui = new GUI();
+		frame.setContentPane(gui.root); // reference: https://www.youtube.com/watch?v=5vSyylPPEko
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
 		try {
-			System.out.println("Connecting to " + serverName + " on port " + port);
-			Socket client = new Socket(serverName, port);
-
-			System.out.println("Just connected to " + client.getRemoteSocketAddress());
-			OutputStream outToServer = client.getOutputStream();
-			this.out = new DataOutputStream(outToServer);
-
+			System.out.println("Connecting to " + serverName + " on port " + portStreaming);
+			Socket client = new Socket(serverName, portStreaming);
 			InputStream inFromServer = client.getInputStream();
 			this.in = new DataInputStream(inFromServer);
+			System.out.println("Just connected to " + client.getRemoteSocketAddress());
+
+			Thread chat = new ChatListener(serverName, portChat, gui, frame);
+			chat.start();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	public void run() {
-
-		JFrame frame = new JFrame("Streaming");
-		GUI gui = new GUI();
-		frame.setContentPane(gui.main); // reference: https://www.youtube.com/watch?v=5vSyylPPEko
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
 
 		while (true) {
 			try {
@@ -48,27 +49,70 @@ public class Client extends Thread {
 				in.readFully(byteImg, 0, len); // reading the screenshot
 
 				BufferedImage img = ImageIO.read(new ByteArrayInputStream(byteImg)); // converting the bytes into an image
-				BufferedImage image = Scalr.resize(img, Scalr.Method.BALANCED,gui.video.getSize().width, gui.video.getSize().height);
+				BufferedImage image = Scalr.resize(img,Scalr.Mode.FIT_TO_HEIGHT,gui.root.getWidth(), (gui.root.getHeight() - gui.chatPanel.getHeight()));
 
 				// showing the image on the GUI
 				if (img != null)
-					gui.jl.setIcon(new ImageIcon(image)); // reference: https://github.com/Imran92/Java-UDP-Video-Stream-Server/blob/master/src/java_video_stream/JavaClient.java#L149
+					gui.label.setIcon(new ImageIcon(image)); // reference: https://github.com/Imran92/Java-UDP-Video-Stream-Server/blob/master/src/java_video_stream/JavaClient.java#L149
+
 				frame.getContentPane().repaint();
 
 				Thread.sleep(15);
 
 			} catch (Exception e) {
 				e.printStackTrace();
+				JOptionPane.showMessageDialog(frame,
+						"Server does not respond.",
+						"ERROR",
+						JOptionPane.WARNING_MESSAGE);
+				break;
+
 			}
 		}
 
 	}
 
 	public static void main(String [] args) {
-		Scanner scan = new Scanner(System.in);
 		String serverName = "localhost";
-		int port = 12345;
-		Thread clt = new Client(serverName, port);
-		clt.start();
+		int portStreaming = 12345;
+		int portChat = 12346;
+		Thread streaming = new Client(serverName, portStreaming, portChat);
+		streaming.start();
+	}
+}
+
+class ChatListener extends Thread {
+
+	private DataOutputStream outChat;
+	private DataInputStream inChat;
+	private JFrame frame;
+	private GUI gui;
+
+	public ChatListener(String serverName, int port, GUI gui, JFrame frame) throws IOException {
+		Socket chat = new Socket(serverName, port);
+		this.outChat = new DataOutputStream(chat.getOutputStream());
+		this.inChat = new DataInputStream(chat.getInputStream());
+		this.gui = gui;
+		this.gui.setUsername(UUID.randomUUID().toString());
+		this.gui.setDataOutputStream(outChat);
+		this.frame = frame;
+	}
+
+	public void run() {
+		while(true) {
+			String chatMessage = null;
+			try {
+				chatMessage = inChat.readUTF();
+				if (chatMessage != null) {
+					gui.view.append(chatMessage);
+					gui.view.setCaretPosition(gui.view.getDocument().getLength());
+				}
+
+				frame.getContentPane().repaint();
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
 	}
 }
--- GUI.java
+++ GUI.java
@@ -1,25 +1,59 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class GUI {
-    public JPanel main;
-    public JLabel jl;
-    public JPanel video;
-    public JScrollPane text;
-    private JTextArea textArea1;
-    private JPanel chat;
+    public JPanel root;
+    public JTextArea view;
     private JButton sendButton;
-    private JTextArea textArea2;
+    public JLabel label;
+    public JPanel chatPanel;
+    private JTextField chat;
+    private DataOutputStream messageToServer;
+    private String username;
 
     public GUI() {
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { // reference: https://github.com/Imran92/Java-UDP-Video-Stream-Server/blob/master/src/java_video_stream/JavaClient.java#L185
+                String sentence = chat.getText();
+                try {
+                    messageToServer.writeUTF(username + ": " + sentence + '\n');
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                chat.setText(null);
+            }
+        });
+        chat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String sentence = chat.getText();
+                try {
+                    messageToServer.writeUTF(username + ": " + sentence + '\n');
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                chat.setText(null);
+            }
+        });
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDataOutputStream(DataOutputStream messageToServer) {
+        this.messageToServer = messageToServer;
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Streaming");
+        JFrame frame = new JFrame("Test");
         GUI gui = new GUI();
-        frame.setContentPane(gui.main); // reference: https://www.youtube.com/watch?v=5vSyylPPEko
+        frame.setContentPane(gui.root); // reference: https://www.youtube.com/watch?v=5vSyylPPEko
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
